50d4f8df5a0e177fa2562678a3ed7b58ade99326
thenyutheta <40385747+thenyutheta@users.noreply.github.com>
2023-10-28 00:06:04 +0900

    fix flow

---
+++ core.js
@@ -0,0 +1,4 @@
+var par = document.getElementById("guide_main");
+var ch = document.createElement("strong");
+ch.textContent = "サービスは終了しました。ご利用ありがとうございました。";
+par.appendChild(ch);
--- launcher.js
+++ launcher.js
@@ -1,4 +1,6 @@
-var par = document.getElementById("guide_main");
-var ch = document.createElement("strong");
-ch.textContent = "サービスは終了しました。ご利用ありがとうございました。";
-par.appendChild(ch);
+var scr_parent = document.getElementById("guide_main");
+var scr_coreurl = (location.protocol == "file:") ? "./core.js" : "https://thenyutheta.github.io/KCW_TRAIN_GUIDE/core.js";
+var scr_core = document.createElement("script");
+scr_core.src = scr_coreurl + '?t=' + Date.now();
+scr_core.onload = function () { };
+scr_parent.appendChild(scr_core);
